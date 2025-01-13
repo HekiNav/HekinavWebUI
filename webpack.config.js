fe16ff5580100a64f8c97a6bf9867df0d52f876d
HekiNav <147833782+HekiNav@users.noreply.github.com>
2025-01-13 23:35:31 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: 'script.js', // Main JavaScript entry point
+  entry: './script.js', // Main JavaScript entry point
   output: {
     filename: 'bundle.js', // Output file name
     path: path.resolve(__dirname, 'dist'), // Output directory
