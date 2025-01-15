6e8d2ff14263a846d181c2ee33d28abfda9617a2
HTV <hvartiainen09@gmail.com>
2025-01-15 18:54:56 +0200

    bit styling

--- src/pages/RoutingPage.vue
+++ src/pages/RoutingPage.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import { Map, Layers, Sources } from "vue3-openlayers";
+import { Map, Layers, Sources, Styles } from "vue3-openlayers";
 import { ref } from "vue";
 import RoutingHome from "../components/RoutingHome.vue";
 import { MVT } from 'ol/format';
 
-
 const zoom = 15;
-const center = [2776349.9688816136, 8438737.417225536];
-const projection = "EPSG:3857";
+const center: Array<GLfloat> = [2776349.9688816136, 8438737.417225536];
+const projection: string = "EPSG:3857";
 const mapUrl = ref("https://digitransit-prod-cdn-origin.azureedge.net/map/v2/hsl-map/{z}/{x}/{y}.png?digitransit-subscription-key=bbc7a56df1674c59822889b1bc84e7ad");
 const stopsUrl = ref("https://digitransit-prod-cdn-origin.azureedge.net/map/v2/finland-stop-map/{z}/{x}/{y}.pbf?digitransit-subscription-key=a1e437f79628464c9ea8d542db6f6e94")
-
 const mvtFormat = new MVT();
+const radius = ref(10);
+const strokeWidth = ref(2);
+const stroke = ref("#ff0000");
+const fill = ref("#ffffff");
+
 </script>
 
 <script lang="ts">
@@ -36,8 +39,17 @@ export default {
         </Layers.OlTileLayer>
 
         <!-- Vector Tile Layer -->
-        <Layers.OlVectorTileLayer>
-          <Sources.OlSourceVectorTile :url="stopsUrl" :format="mvtFormat"></Sources.OlSourceVectorTile>
+        <Layers.OlVectorTileLayer :style="null">
+          <Sources.OlSourceVectorTile :url="stopsUrl" :format="mvtFormat" />
+          <Styles.OlStyle>
+            <Styles.OlStyleCircle :radius="radius">
+              <Styles.OlStyleFill :color="fill" />
+              <Styles.OlStyleStroke
+                :color="stroke"
+                :width="strokeWidth"
+              ></Styles.OlStyleStroke>
+            </Styles.OlStyleCircle>
+          </Styles.OlStyle>
         </Layers.OlVectorTileLayer>
       </Map.OlMap>
     </div>
